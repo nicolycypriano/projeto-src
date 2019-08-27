6abfd9eb86059d1c8e2344f3e4d8f53cf610f00f
Nicoly Alves Cypriano <nicoly.cypriano@digitalpages.com.br>
2019-08-27 13:08:05 -0300

    Comitando pasta components

---
+++ FRONTEND/src/components/Error/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ErrorStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  flex: 1;
+  padding: 20px;
+  align-items: center;
+  align-self: center;
+  justify-content: center;
+  font-weight: bold;
+  color: #900;
+`;
