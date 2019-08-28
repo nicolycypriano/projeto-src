2c31831324a16f5ab565a3c3009c1b0f0482fd2a
Nicoly Alves Cypriano <nicoly.cypriano@digitalpages.com.br>
2019-08-27 22:08:05 -0300

    Comitando app

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import './App.css'
+
+import Button from './components/Button'
 
 function App() {
-    return <h1>Hello World</h1>
+    return <Button />
 }
 
 export default App
