31bc072e0579b45503fb8cdd8504cc91a98bb98c
Nicoly Alves Cypriano <nicoly.cypriano@digitalpages.com.br>
2019-09-16 12:51:02 -0300

    Comitando routes

--- FRONTEND/src/routes/routes.js
+++ FRONTEND/src/routes/routes.js
@@ -1,8 +1,23 @@
-import React from "react";
-import { Switch, BrowserRouter } from "react-router-dom";
+import React from 'react'
+import { Switch, BrowserRouter, Route } from 'react-router-dom'
 
-const Router = () => {
-    <BrowserRouter>
-        <AdminProtectedRoute exact path="/home" component={AdminHome} />
-    </BrowserRouter>
+//Importar páginas
+import Main from '../pages/Main/index'
+import Repository from '../pages/Repository/index'
+
+// const Router = () => {
+//     <BrowserRouter>
+//         <AdminProtectedRoute exact path="/home" component={AdminHome} />
+//     </BrowserRouter>
+// }
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Main} />
+                <Route path="/repo" component={Repository} />
+            </Switch>
+        </BrowserRouter>
+    )
 }
