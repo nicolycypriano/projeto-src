61ef7b369446cd825755210eff400255c80969e1
Nicoly Alves Cypriano <nicoly.cypriano@digitalpages.com.br>
2019-09-16 12:49:41 -0300

    Comitando componente de animação da page home (main)

---
+++ FRONTEND/src/pages/Main/Components/BackgroundAnimate/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+import { Area, Circles } from './styles'
+
+class BackgroundAnimate extends Component {
+    render() {
+        return (
+            <Area>
+                <Circles>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </Circles>
+            </Area>
+        )
+    }
+}
+
+export default BackgroundAnimate
