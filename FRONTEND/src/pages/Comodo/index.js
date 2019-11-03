a728b6d37ba4f5a0ba2549b317cb86b4f8865786
nicolycypriano <nicolycypriano@gmail.com>
2019-11-03 15:25:38 -0300

    Alterando index de comodo

--- FRONTEND/src/pages/Comodo/index.js
+++ FRONTEND/src/pages/Comodo/index.js
@@ -1,18 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { H1Styled, Select, Button } from './styles';
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
-function Comodo() {
-  return (
-    <>
-      <H1Styled>Adicionar novo cômodo</H1Styled>
-      <Select name="tipo" placeholder="Nome do cômodo" />
+class Comodo extends Component {
 
-      <Link to="/projeto">
-        <Button>Inserir comodo</Button>
-      </Link>
-    </>
-  );
+  state = {
+    residencia: null,
+    componentes: [],
+    comodo: null
+  }
+
+  componentDidMount() {
+    this.setState({ residencia: this.props.location.state.residencia });
+    api.get('/componentes')
+      .then(response => {
+        this.setState({ componentes: response.data.tipoComodo });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  handleChange = e => {
+    this.setState({ comodo: e.target.value });
+  }
+
+  cadastraComodo = () => {
+    console.log(this.state)
+
+    api.post('/comodo/create', {
+      'residencia': this.state.residencia,
+      'tipoComodo': this.state.comodo
+    })
+      .then(response => {
+        //this.setState({ componentes: response.data.tipoComodo});
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <H1Styled>Adicionar novo cômodo</H1Styled>
+        <Select name="tipo" placeholder="Nome do cômodo" onChange={this.handleChange}>
+          {this.state.componentes.map((opcao) => <option key={opcao.id} value={opcao.id}>{opcao.nome}</option>) || <option>Selecione um cômodo</option>}
+        </Select>
+
+        <Link to="/projeto">
+          <Button onClick={this.cadastraComodo}>Inserir comodo</Button>
+        </Link>
+      </>
+    );
+  }
+
 }
 
 export default Comodo;
