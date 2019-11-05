89eff7900ac2d8353ac292e7cdb477a142ce5cd1
nicolycypriano <nicolycypriano@gmail.com>
2019-11-04 22:18:03 -0300

    Comitando tela de acessar

--- FRONTEND/src/pages/SignIn/index.js
+++ FRONTEND/src/pages/SignIn/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
@@ -7,32 +7,41 @@ import * as Yup from 'yup';
 import { signInRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/logo.svg';
+import { Container } from '~/components/Header/styles';
+import { Button } from '../Residencia/styles';
 
-const schema = Yup.object().shape({
-  email: Yup.string()
-    .email('Email inválido!')
-    .required('E-mail é obrigatório'),
-  password: Yup.string().required('A senha é obrigatória.'),
-});
+// const schema = Yup.object().shape({
+//   email: Yup.string()
+//     .email('Email inválido!')
+//     .required('E-mail é obrigatório'),
+//   password: Yup.string().required('A senha é obrigatória.'),
+// });
 
-export default function SignIn() {
-  const dispatch = useDispatch();
-  const loading = useSelector(state => state.auth.loading);
+// export default function SignIn() {
+//   const dispatch = useDispatch();
+//   const loading = useSelector(state => state.auth.loading);
 
-  function handleSubmit({ email, password }) {
-    dispatch(signInRequest(email, password));
+//   function handleSubmit({ email, password }) {
+//     dispatch(signInRequest(email, password));
+//   }
+class SignIn extends Component {
+  render() {
+    return (
+      <>
+        <img src={logo} alt="SRC"/>
+        <Form>
+          <Input name="email" type="email" placeholder="Insira seu e-mail" />
+          <Input name="password" type="password" placeholder="Insira sua senha" />
+
+          <Link to="/residencia/list">
+            <Button type="submit">Acessar</Button>
+          </Link>
+          <Link to="/criarconta">Criar conta gratuita</Link>
+        </Form>
+      </>
+    );
   }
-
-  return (
-    <>
-      <img src={logo} alt="GoBarber" />
-      <Form schema={schema} onSubmit={handleSubmit}>
-        <Input name="email" type="email" placeholder="Insira seu e-mail" />
-        <Input name="password" type="password" placeholder="Insira sua senha" />
-
-        <button type="submit">{loading ? 'Carregando...' : 'Acessar'}</button>
-        <Link to="/register">Criar conta gratuita</Link>
-      </Form>
-    </>
-  );
 }
+
+export default SignIn
+
