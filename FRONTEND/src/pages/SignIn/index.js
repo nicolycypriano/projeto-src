d791584e385c98c51f60d41a4c414e461dd11c3a
nicolycypriano <nicolycypriano@gmail.com>
2019-11-06 20:13:08 -0300

    Arrumando sign in, ainda em alterações

--- FRONTEND/src/pages/SignIn/index.js
+++ FRONTEND/src/pages/SignIn/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import { signInRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/logo.svg';
+import { Img } from './styles';
+import { Container } from './styles';
 
 const schema = Yup.object().shape({
   email: Yup.string()
     .email('Email inválido!')
     .required('E-mail é obrigatório'),
-  password: Yup.string().required('A senha é obrigatória.'),
+  password: Yup.string().required('A senha é obrigatória.')
 });
 
 export default function SignIn() {
@@ -25,14 +26,21 @@ export default function SignIn() {
 
   return (
     <>
-      <img src={logo} alt="GoBarber" />
-      <Form schema={schema} onSubmit={handleSubmit}>
-        <Input name="email" type="email" placeholder="Insira seu e-mail" />
-        <Input name="password" type="password" placeholder="Insira sua senha" />
+      <Container>
+        <Img src={logo} alt="SRC" />
 
-        <button type="submit">{loading ? 'Carregando...' : 'Acessar'}</button>
-        <Link to="/register">Criar conta gratuita</Link>
-      </Form>
+        <form schema={schema} onSubmit={handleSubmit}>
+          <input name="email" type="email" placeholder="Insira seu e-mail" />
+          <input
+            name="password"
+            type="password"
+            placeholder="Insira sua senha"
+          />
+
+          <button type="submit">{loading ? 'Carregando...' : 'Acessar'}</button>
+          <Link to="/register">Criar conta gratuita</Link>
+        </form>
+      </Container>
     </>
   );
 }
---
+++ FRONTEND/src/pages/SignIn/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Img = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  button {
+    border-radius: 2px;
+    background: #222;
+    color: #fff;
+    padding: 10px;
+    margin: 10px 0 0 0;
+    border: none;
+    background: #7159c1;
+  }
+`;
