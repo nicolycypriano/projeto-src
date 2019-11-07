9dd542f402607d9f27155028d9b70fc858b56266
nicolycypriano <nicolycypriano@gmail.com>
2019-11-06 21:26:03 -0300

    Comitando página de login

--- FRONTEND/src/pages/SignIn/styles.js
+++ FRONTEND/src/pages/SignIn/styles.js
@@ -3,13 +3,18 @@ import styled from 'styled-components';
 export const Img = styled.img`
   width: 100px;
   height: 100px;
+  margin-bottom: 10px;
 `;
 
 export const Container = styled.div`
-  display: flex;
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
+  display: flex;
+  font-size: 15px;
 
   button {
     border-radius: 2px;
@@ -18,6 +23,29 @@ export const Container = styled.div`
     padding: 10px;
     margin: 10px 0 0 0;
     border: none;
-    background: #7159c1;
+    background: rgb(113, 52, 161);
+    margin-bottom: 10px;
   }
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+
+  input {
+    width: 100%;
+    height: 30px;
+    display: flex;
+    border-radius: 2px;
+    border: none;
+    padding-left: 15px;
+    font-size: 16px;
+    margin-bottom: 10px;
+  }
+`;
+
+export const ContainerForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
 `;
