7300ad8d03485dec8e74573d16ea25ec9288bcd9
Nicoly Alves <nicolycypriano@gmail.com>
2019-11-24 17:47:42 -0300

    Enviando props para dentro dos componentes

--- FRONTEND/src/routes/index.js
+++ FRONTEND/src/routes/index.js
@@ -25,18 +25,18 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" component={SignUp} />
-      <PrivateRoute path="/comodo/edit/:id" exact component={() => <ComodoEditar/>} />
-      <PrivateRoute path="/residencia/:idResidencia/comodo/:idComodo/atuador" exact component={() => <Atuador/>} />
-      <PrivateRoute path="/residencia/:idResidencia/comodo/:idComodo/sensor" exact component={() => <Sensor/>} />
-      <PrivateRoute path="/profile" exact component={() => <Profile/>} />
-      <PrivateRoute path="/residencia/:id/comodo" exact component={() => <Comodo/>} />
-      <PrivateRoute path="/residencia/list" exact component={() => <ListagemResidencia/>} />
-      <PrivateRoute path="/residencia/edit/:id" exact component={() => <ResidenciaEditar/>} />     
-      <PrivateRoute path="/residencia" exact component={() => <Residencia/>} />
-      <PrivateRoute path="/componentes/residencia/:idResidencia/comodo/:idComodo/sensor/edit/:id" exact component={() => <SensorEditar/>} />
-      <PrivateRoute path="/componentes/residencia/:idResidencia/comodo/:idComodo/atuador/edit/:id" exact component={() => <AtuadorEditar/>} />
-      <PrivateRoute path="/comodo/list/:id" exact component={() => <ComodoList/>} />
-      <PrivateRoute path="/componentes/residencia/:idResidencia/comodo/:idComodo" exact component={() => <Componentes/>} />
+      <PrivateRoute path="/comodo/edit/:id" exact component={props => <ComodoEditar {...props}/>} />
+      <PrivateRoute path="/residencia/:idResidencia/comodo/:idComodo/atuador" exact component={props => <Atuador {...props}/>} />
+      <PrivateRoute path="/residencia/:idResidencia/comodo/:idComodo/sensor" exact component={props => <Sensor {...props}/>} />
+      <PrivateRoute path="/profile" exact component={props => <Profile {...props}/>} />
+      <PrivateRoute path="/residencia/:id/comodo" exact component={props => <Comodo {...props}/>} />
+      <PrivateRoute path="/residencia/list" exact component={props => <ListagemResidencia {...props}/>} />
+      <PrivateRoute path="/residencia/edit/:id" exact component={props => <ResidenciaEditar {...props}/>} />     
+      <PrivateRoute path="/residencia" exact component={props => <Residencia {...props}/>} />
+      <PrivateRoute path="/componentes/residencia/:idResidencia/comodo/:idComodo/sensor/edit/:id" exact component={props => <SensorEditar {...props}/>} />
+      <PrivateRoute path="/componentes/residencia/:idResidencia/comodo/:idComodo/atuador/edit/:id" exact component={props => <AtuadorEditar {...props}/>} />
+      <PrivateRoute path="/comodo/list/:id" exact component={props => <ComodoList {...props} />} />
+      <PrivateRoute path="/componentes/residencia/:idResidencia/comodo/:idComodo" exact component={props => <Componentes {...props}/>} />
       <Route path="*" component={() => <h1>Page not found</h1>} />
     </Switch>
   );
